2dbd27f5fc3ec73203872863aba9971e557715cc
Vignesh Natarajan <viki@vikilabs.org>
2019-04-19 20:44:17 -0400

    installation scripts fix

--- compile_cpu.sh
+++ compile_cpu.sh
@@ -1,4 +1,4 @@
-cd ../
+cd src/
 #MY GPU NVIDIA NVS 4200 M [ CUDA 9.0 + NVIDIA 384]
 cp ./Makefile_cpu ./Makefile
 make clean
--- compile_gpu.sh
+++ compile_gpu.sh
@@ -1,4 +1,4 @@
-cd ../
+cd src/
 cp ./Makefile_gpu ./Makefile
 make clean
 make
